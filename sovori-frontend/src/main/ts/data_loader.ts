182e8e415b5da187faed43cb39537128e4886fab
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2018-01-29 23:12:00 +0100

    j -> äu

--- sovori-frontend/src/main/ts/data_loader.ts
+++ sovori-frontend/src/main/ts/data_loader.ts
@@ -1,19 +1,19 @@
 export class DataRecord {
-    q: string;
-    a: string;
+  q: string;
+  a: string;
 
-    get question(): string {
-        return this.q;
-    }
+  get question(): string {
+    return this.q;
+  }
 }
 
 export class DataLoaderResponse {
-    data: DataRecord[];
+  data: DataRecord[];
 }
 
 export default class DataLoader {
 
-    async load(): Promise<DataLoaderResponse> {
-        return fetch("/data/data.json").then(r => r.json());
-    }
+  async load(): Promise<DataLoaderResponse> {
+    return fetch("/data/data.json").then(r => r.json());
+  }
 }
--- sovori-frontend/src/main/ts/test.ts
+++ sovori-frontend/src/main/ts/test.ts
@@ -1,87 +1,90 @@
 import {DataRecord} from "./data_loader.js";
 
 export class Test {
-    data: DataRecord[] = [];
-    questionElement: Element;
-    answerElement: HTMLTextAreaElement;
-    private currentQuestion: DataRecord;
+  data: DataRecord[] = [];
+  questionElement: Element;
+  answerElement: HTMLTextAreaElement;
+  private currentQuestion: DataRecord;
 
-    constructor(data: DataRecord[]) {
-        this.data = data;
-        this.questionElement = <Element><any>document.getElementById("question");
-        this.answerElement = <HTMLTextAreaElement><any>document.getElementById("answer");
+  constructor(data: DataRecord[]) {
+    this.data = data;
+    this.questionElement = <Element><any>document.getElementById("question");
+    this.answerElement = <HTMLTextAreaElement><any>document.getElementById("answer");
 
-        this.initEvents();
-    }
+    this.initEvents();
+  }
 
-    private initEvents() {
-        this.answerElement.addEventListener("keydown", evt => {
-            if (evt.metaKey) {
-                if (evt.altKey) {
-                    let isUppercase = evt.shiftKey || evt.getModifierState("CapsLock");
-                    if (evt.code == "KeyS") {
-                        evt.preventDefault();
-                        if (this.answerElement.value.endsWith("sch")) {
-                            this.answerElement.value = this.answerElement.value.replace(/sch$/, 'ß');
-                        } else {
-                            this.answerElement.value += isUppercase ? 'Sch' : 'sch';
-                        }
-                    } else if (evt.code == "KeyT") {
-                        evt.preventDefault();
-                        this.answerElement.value += isUppercase ? 'Tsch' : 'tsch';
-                    } else if (evt.code == "KeyO") {
-                        evt.preventDefault();
-                        this.answerElement.value += isUppercase ? 'Ö' : 'ö';
-                    } else if (evt.code == "KeyU") {
-                        evt.preventDefault();
-                        this.answerElement.value += isUppercase ? 'Ü' : 'ü';
-                    } else if (evt.code == "KeyA") {
-                        evt.preventDefault();
-                        this.answerElement.value += isUppercase ? 'Ä' : 'ä';
-                    } else if (evt.code == "KeyX") {
-                        evt.preventDefault();
-                        this.answerElement.value += isUppercase ? 'Ch' : 'ch';
-                    } else if (evt.code == "KeyE") {
-                        evt.preventDefault();
-                        this.answerElement.value += isUppercase ? 'Ei' : 'ei';
-                    }
-                }
-                if (evt.code == "Period") {
-                    this.ask();
-                } else if (evt.code === "Slash") {
-                    this.hint();
-                } else if (evt.key === "Enter") {
-                    this.check(this.answerElement.value);
-                }
-            }
-        });
-    }
-
-    private nextQuestion(): DataRecord {
-        return this.data[Math.floor(Math.random() * this.data.length)]
-    }
-
-    private check(answer: string) {
-        if (this.currentQuestion) {
-            if (answer !== this.currentQuestion.a) {
-                this.questionElement.classList.add("is-incorrect");
+  private initEvents() {
+    this.answerElement.addEventListener("keydown", evt => {
+      if (evt.metaKey) {
+        if (evt.altKey) {
+          let isUppercase = evt.shiftKey || evt.getModifierState("CapsLock");
+          if (evt.code == "KeyS") {
+            evt.preventDefault();
+            if (this.answerElement.value.endsWith("sch")) {
+              this.answerElement.value = this.answerElement.value.replace(/sch$/, 'ß');
             } else {
-                this.questionElement.classList.add("is-correct");
-                this.ask();
+              this.answerElement.value += isUppercase ? 'Sch' : 'sch';
             }
+          } else if (evt.code == "KeyT") {
+            evt.preventDefault();
+            this.answerElement.value += isUppercase ? 'Tsch' : 'tsch';
+          } else if (evt.code == "KeyO") {
+            evt.preventDefault();
+            this.answerElement.value += isUppercase ? 'Ö' : 'ö';
+          } else if (evt.code == "KeyU") {
+            evt.preventDefault();
+            this.answerElement.value += isUppercase ? 'Ü' : 'ü';
+          } else if (evt.code == "KeyA") {
+            evt.preventDefault();
+            this.answerElement.value += isUppercase ? 'Ä' : 'ä';
+          } else if (evt.code == "KeyX") {
+            evt.preventDefault();
+            this.answerElement.value += isUppercase ? 'Ch' : 'ch';
+          } else if (evt.code == "KeyE") {
+            evt.preventDefault();
+            this.answerElement.value += isUppercase ? 'Ei' : 'ei';
+          } else if (evt.code == "KeyJ") {
+            evt.preventDefault();
+            this.answerElement.value += isUppercase ? 'Äu' : 'äu';
+          }
         }
-    }
-
-    private hint() {
-        if (this.currentQuestion) {
-            this.answerElement.value = this.currentQuestion.a;
+        if (evt.code == "Period") {
+          this.ask();
+        } else if (evt.code === "Slash") {
+          this.hint();
+        } else if (evt.key === "Enter") {
+          this.check(this.answerElement.value);
         }
-    }
+      }
+    });
+  }
 
-    ask() {
-        this.currentQuestion = this.nextQuestion();
-        this.answerElement.value = "";
-        this.questionElement.textContent = this.currentQuestion.q;
-        this.answerElement.focus();
+  private nextQuestion(): DataRecord {
+    return this.data[Math.floor(Math.random() * this.data.length)]
+  }
+
+  private check(answer: string) {
+    if (this.currentQuestion) {
+      if (answer !== this.currentQuestion.a) {
+        this.questionElement.classList.add("is-incorrect");
+      } else {
+        this.questionElement.classList.add("is-correct");
+        this.ask();
+      }
     }
+  }
+
+  private hint() {
+    if (this.currentQuestion) {
+      this.answerElement.value = this.currentQuestion.a;
+    }
+  }
+
+  ask() {
+    this.currentQuestion = this.nextQuestion();
+    this.answerElement.value = "";
+    this.questionElement.textContent = this.currentQuestion.q;
+    this.answerElement.focus();
+  }
 }
