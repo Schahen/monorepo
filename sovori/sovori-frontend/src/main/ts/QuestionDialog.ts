b8513326d1cf926a71d72f311e2eee85b785bf2d
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2018-04-28 19:56:05 +0200

    [sovori-frontend] better layout for question dialog

--- sovori/sovori-frontend/src/main/ts/QuestionDialog.ts
+++ sovori/sovori-frontend/src/main/ts/QuestionDialog.ts
@@ -20,8 +20,7 @@ export class QuestionDialog {
     this.closeButton.addEventListener("click", evt => {
       this.close();
     });
-
-  }
+    }
 
    onSave(handler: (question: string) => void ) {
      this.saveButton.addEventListener("click", evt => {
