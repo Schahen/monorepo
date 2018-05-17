cc36cd523d8349bf6d82b7c754f345f75280a7ed
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2018-05-17 23:18:00 +0200

    [sovori-backend] miniman websocket inialization

--- sovori/sovori-backend/src/main/ts/app.ts
+++ sovori/sovori-backend/src/main/ts/app.ts
@@ -1,10 +1,24 @@
 import * as express from 'express';
 import * as path from 'path';
 import {coursesRouter} from "./api/coursesRouter";
+import {Server as WebsocketServer} from "ws";
 
 let registerPath = (app: express.Application, path: string, resolveTo: string) => {
   console.log(`${path} => ${resolveTo}`)
   app.use(<any>path, express.static(resolveTo));
+}
+
+
+let websocketServer = (port: number) => {
+  const webSocketServer = new WebsocketServer({ port });
+
+  webSocketServer.on('connection', function connection(webSocket) {
+
+    webSocket.on('message', function incoming(message) {
+
+    });
+
+  });
 }
 
 let main = () => {
@@ -24,6 +38,8 @@ let main = () => {
   app.listen(port, function () {
     console.log(`app listening on port ${port}!`);
   });
+
+  websocketServer(4001);
 }
 
 
