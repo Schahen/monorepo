7db4c85542e2d20e4b292fe185e21f282fe7ea30
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2018-05-22 22:10:09 +0200

    [sovori] rightMetaKey, leftMetaKey introduced (not used anywhere so far)

--- sovori/sovori-frontend/src/main/ts/events/InputEvent.ts
+++ sovori/sovori-frontend/src/main/ts/events/InputEvent.ts
@@ -1,12 +1,27 @@
 import {KeyDownEvent} from "./KeyDownEvent.js";
 import {RegisteredEvent} from "./RegisteredEvent.js";
-import {registerKeydown} from "./addRegisteredEvent.js";
+import {registerKeydown, withKeyDown} from "./addRegisteredEvent.js";
 import {LetterHandler} from "../keyboard/LetterHandler.js";
 
 
 export module InputEvent {
 
   export let keyDown = (el: Element): RegisteredEvent<KeyDownEvent> => {
+
+    let rightMeta = true;
+    let leftMeta = true;
+
+    withKeyDown(el).on(evt => {
+      if (evt.metaKey) {
+        if (evt.location == 1) {
+          leftMeta = true;
+          rightMeta = false;
+        } else if (evt.location == 2) {
+          rightMeta = true;
+          leftMeta = false;
+        }
+      }
+    });
 
     return registerKeydown<KeyDownEvent>(el,  evt => {
 
@@ -17,6 +32,8 @@ export module InputEvent {
        metaKey: evt.metaKey,
        shiftKey: evt.shiftKey,
        capsLock: evt.getModifierState("CapsLock"),
+       rightMetaKey: evt.metaKey && rightMeta,
+       leftMetaKey: evt.metaKey && leftMeta,
        preventDefault: () => evt.preventDefault()
      }
     });
@@ -24,6 +41,8 @@ export module InputEvent {
 
   export function resolveLetter(event: RegisteredEvent<KeyDownEvent>, handler: LetterHandler): RegisteredEvent<string|undefined> {
     return event.on(evt => {
+      console.log(evt);
+
       if (evt.metaKey) {
         if (evt.altKey) {
           let isUppercase = evt.shiftKey || evt.capsLock;
